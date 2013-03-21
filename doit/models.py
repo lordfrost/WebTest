484f59ef5708559ee474dca062862ee87fc17def
lordfrost <xblp@xakep.ru>
2013-03-21 23:38:12 +0600

    thats right!

--- doit/models.py
+++ doit/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib import admin
 
 class BlogPost(models.Model):
+    class Meta:
+        ordering = ('-timestamp',)
     title = models.CharField(max_length=150)
     body = models.TextField()
     timestamp = models.DateTimeField()
